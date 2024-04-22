8d1f6c1bfc402ce4cadf14d72a237924462d894b
mdss00 <miguids00@gmail.com>
2024-04-22 09:43:35 +0200

    🔧 feat(SimbolosRomanos.java): Add new enum class

---
+++ app/src/main/java/edu/badpals/romans/SimbolosRomanos.java
@@ -0,0 +1,15 @@
+package edu.badpals.romans;
+
+public enum SimbolosRomanos {
+    I(1), V(5), X(10), L(50), C(100), D(500), M(1000);
+
+    private int valorDecimal;
+
+    private  SimbolosRomanos(int valorDecimal){
+        this.valorDecimal = valorDecimal;
+    }
+
+    public int getValorDecimal() {
+        return valorDecimal;
+    }
+}
